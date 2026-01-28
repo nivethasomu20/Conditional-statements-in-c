cf2e8cc5023315347b238a4089e06f5fd7e11be3
Nivetha Somu <nivethasomu20@gmail.com>
2026-01-28 21:10:34 +0530

    Add files via upload

---
+++ problem2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int mark;
+    scanf("%d",&mark);
+    switch(mark/10){
+        case 9:
+            printf("Grade A");
+            break;
+        case 8:
+            printf("Grade B");
+            break;
+        case 7:
+            printf("Grade C");
+            break;
+        case 6:
+            printf("Grade D");
+            break;
+        case 5:
+            printf("Grade E");
+            break;
+        case 4:
+            printf("Grade F");
+            break;
+        case 3:
+            printf("Grade supplementary");
+            break;
+        case 2:
+            printf("Fail");
+    }
+          return 0;
+    }
